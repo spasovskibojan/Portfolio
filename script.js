7c294df7402c7c69d63edde35795d3cf4e0169c8
spasovskibojan <bojanspasovski04@gmail.com>
2025-02-21 14:37:09 +0100

    Update portfolio: fixed HTML, CSS, and updated images

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    // event.preventDefault();
+// Navbar dropdown
+const navbar = document.getElementById('navbar');
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+function closeNavbarOnClickOutside(event) {
+    // Check if the click was outside the navbar or dropdown
+    if (!navbar.contains(event.target) && !dropdown.contains(event.target)) {
+        dropdownContent.style.display = 'none';
+    }
+}
+document.addEventListener('click', closeNavbarOnClickOutside);
+const dropdownButton = dropdown.querySelector('.dropbtn');
+dropdownButton.addEventListener('click', function (event) {
+    event.stopPropagation();
 
-    if(name && email && message) {
-        alert('Thank you for reaching out! I will contact you soon.');
-        document.getElementById('contact-form').reset();
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
     } else {
-        alert('Please fill out all fields.');
+        dropdownContent.style.display = 'block';
     }
 });
-document.addEventListener('DOMContentLoaded', (event)=>{
+
+// Navbar change active site
+document.addEventListener('DOMContentLoaded', (event) => {
     const currentLocation = location.href
     const menuItem = document.querySelectorAll('.navbar .nav-right li a')
-    menuItem.forEach(item =>{
+    console.log(menuItem)
+    menuItem.forEach(item => {
         item.classList.remove('active')
-        if(item.href==currentLocation){
+        if (item.href == currentLocation) {
             item.classList.add('active')
         }
     })
-
 })
+
+// Contact form
+if (location.href == 'contact.html') {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (name && email && message) {
+            alert('Thank you for reaching out! I will contact you soon.');
+            document.getElementById('contact-form').reset();
+        } else {
+            alert('Please fill out all fields.');
+        }
+    });
+}
+
+
+// Work script
+
